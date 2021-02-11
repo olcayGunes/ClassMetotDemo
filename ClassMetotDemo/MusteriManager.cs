22dbbcab49415a507e146570d9842932ce780930
olcayGunes <65913895+olcayGunes@users.noreply.github.com>
2021-02-11 13:14:43 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+	class MusteriManager
+	{
+		public void Ekle(Musteri musteri)
+		{
+			Console.WriteLine(musteri.Ad+" "+musteri.Soyad+" isimli müşteri sisteme eklenmiştir.");
+		}
+
+		public void Sil(Musteri musteri)
+		{
+			Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " isimli müşteri sistemden silinmiştir.");
+		}
+
+		public void Listele(Musteri[] musteriler)
+		{
+			foreach (var musteri in musteriler)
+			{
+				Console.WriteLine("Müşteri Adı		: " + musteri.Ad);
+				Console.WriteLine("Müşteri Soyadı		: " + musteri.Soyad);
+				Console.WriteLine("Müşteri Hesap No	: " + musteri.HesapNo);
+				Console.WriteLine("-----------------------------------------");
+			}
+		}
+	}
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Musteri classından 5 adet musteri nesnesi oluşturuyoruz.
+			Musteri musteri1 = new Musteri { Id = 1, Ad = "Olcay", Soyad = "Güneş", HesapNo = "1111111" };
+			Musteri musteri2 = new Musteri { Id = 2, Ad = "Ahmet", Soyad = "Ercan", HesapNo = "1111122" };
+			Musteri musteri3 = new Musteri { Id = 3, Ad = "Mehmet", Soyad = "Demir", HesapNo = "1312312" };
+			Musteri musteri4 = new Musteri { Id = 4, Ad = "Ali", Soyad = "Yılmaz", HesapNo = "1451111" };
+			Musteri musteri5 = new Musteri { Id = 5, Ad = "Mustafa", Soyad = "Türkmen", HesapNo = "1654111" };
+
+			// Oluşturduğumuz musteri nesnelerinden bir dizi oluşturup nesneleri içine atıyoruz.(Listelemede kullanmak için)
+			Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4, musteri5 };
+
+			// MusteriManager classının bir örneğini oluşturuyoruz.
+			MusteriManager musteriManager = new MusteriManager();
+			musteriManager.Listele(musteriler);
+
+			musteriManager.Ekle(musteri1);
+
+			musteriManager.Sil(musteri4);
+		}
+	}
+}
